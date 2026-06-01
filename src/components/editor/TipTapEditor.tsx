cce1067183f658ca07daad0e975a92372e7fde12
Ezekia <ezekiaawardk@gmail.com>
2026-06-01 18:18:36 +0300

    Add TipTap editor placeholder component

---
+++ src/components/editor/TipTapEditor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TipTapEditor() {
+  return (
+    <div className="h-full w-full">
+      <div className="border rounded-lg h-full p-4 text-gray-400">
+        TipTap editor will be initialized here.
+      </div>
+    </div>
+  );
+}
